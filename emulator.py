4ced7ab42ec1c935242a3baf6f4a06dbe2335f0b
PrasunGuragain <guragain@wisc.edu>
2023-11-02 15:54:36 -0500

    emulator routing, send and log_event done

--- emulator.py
+++ emulator.py
@@ -1,6 +1,47 @@
 import socket
 import sys
+import struct
+import random
+import time
 
+priority_list = {"1": [], "2": [], "3": []}
+
+def log_event(log, message):
+    with open(log, 'a') as file:
+        file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
+
+def send(next_hop, delay, loss_probability, log):
+    if random.random() * 100 > loss_probability:
+        time.sleep(delay / 1000)
+        
+        # send packet to next hop since no loss
+        
+        
+    else:
+        message = "loss event occurred"
+        log_event(log, message)
+        pass
+    
+def routing(priority, src_ip_address, src_port, dest_ip_address, dest_port, length, packet_type, sequence_number, payload_length, payload, sock, log):
+    dest_found = False
+    with open("table.txt", 'r') as file:
+        for line in file:
+            columns = line.strip().split()
+            emulator, e_port = columns[0:2]
+            destination, d_port = columns[2:4]
+            next_hop, next_port = columns[4:6]
+            delay = columns[6]
+            loss_probability = columns[7]
+            
+            if dest_port == d_port:
+                send(next_hop, delay, loss_probability, sock, log)
+                dest_found = True
+                
+    if not dest_found:
+        message = "no forwarding entry found"
+        log_event(log, message)
+
+            
 # PARSE COMMAND LINE ARGUMENTS
     
 # Port
@@ -39,7 +80,27 @@ else:
     sys.exit(1)
 
 # Create a socket for the requester
-requester_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-requester_socket.bind(('0.0.0.0', port))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', port))
 
-# Listen from server
+# Receive a packet
+packet, addr = sock.recvfrom(5000)
+
+priority = struct.unpack('B', packet[:1])
+src_ip_address = struct.unpack('4s', packet[1:5])
+src_port = struct.unpack('H', packet[5:7])
+dest_ip_address = struct.unpack('4s', packet[7:11])
+dest_port = struct.unpack('H', packet[11:13])
+length = struct.unpack('I', packet[13:17])
+
+# payload
+packet_type = struct.unpack('c', packet[17:18])
+sequence_number = struct.unpack('I', packet[18:22])
+payload_length = struct.unpack('I', packet[22:26])
+payload = packet[26:26 + payload_length]
+
+# Decide where it is to be forwarded
+routing(priority, src_ip_address, src_port, dest_ip_address, dest_port, length, packet_type, sequence_number, payload_length, payload, sock, log)
+send()
+
+
